01ae5e0567db068d84291abe5cc677e0ca82371f
oleg.ch <oleg.ch@sunrise-r.ru>
2022-08-07 13:47:27 +0300

    git init

---
+++ src/main/java/ru/home/preparado/PreparadoApplication.java
@@ -0,0 +1,13 @@
+package ru.home.preparado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/home/preparado/PreparadoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.preparado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

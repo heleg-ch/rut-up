15966ca01e3a06ad6624c280f35a801ad1985cbc
oleg.ch <oleg.ch@sunrise-r.ru>
2022-08-08 16:29:45 +0300

    add check login

--- src/main/resources/static/js/action.js
+++ src/main/resources/static/js/action.js
@@ -1,3 +1,28 @@
 function showAlert() {
     alert("The button was clicked!");
 }
+
+function back() {
+    window.history.back();
+}
+
+function checkLogin() {
+    const login = {
+        login: document.getElementById("login").value
+    }
+
+    post('/api/sign-up', JSON.stringify(login));
+}
+
+function post(url, body) {
+    const request = new XMLHttpRequest();
+    request.open('POST',  url, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.onload = function () {
+        console.log(this.response);
+    };
+    request.onerror = function () {
+        console.log("error");
+    }
+    request.send(body);
+}
